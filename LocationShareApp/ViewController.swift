540f6a5b8f33ecb75b0bcce014ed1fab3e3cc07d
fummicc1 <fumiya.tennis1@gmail.com>
2019-08-26 20:09:04 +0900

    commit

--- LocationShareApp/ViewController.swift
+++ LocationShareApp/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
     }
-
-
+    
+    @IBAction func reloadButton() {
+        let url = URL(string: "https://")!
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                fatalError(error.localizedDescription)
+            }
+            DispatchQueue.main.async {
+                let alert = UIAlertController(title: "更新完了", message: "", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
 }
-
